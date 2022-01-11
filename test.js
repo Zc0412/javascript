b1f4dff91dd221c3c204dafb1672ddf0ca210cff
lessismore <zhangmingcheng0412@gmail.com>
2022-01-11 23:28:17 +0800

    js

---
+++ test.js
@@ -0,0 +1,10 @@
+const data = {
+    data1:[{id:1,name:'cat1'},{id:2,name:'cat2'}],
+    data2:[{id:3,name:'cat3'},{id:4,name:'cat4'}],
+    data4:[{id:5,name:'cat5'},{id:6,name:'cat6'}],
+}
+const data5 ={
+    data2:'cat3'
+}
+console.log(data5[data2])
+console.log(data[data2])
